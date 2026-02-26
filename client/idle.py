71515040f54e8d9fb658d7340498f1be04b79e95
hihancioglu <ibrahim.hancioglu@gmail.com>
2026-02-26 15:21:38 +0300

    Refactor Windows idle client into modules and state machine

---
+++ client/idle.py
@@ -0,0 +1,25 @@
+import ctypes
+from ctypes import wintypes
+
+
+class LASTINPUTINFO(ctypes.Structure):
+    _fields_ = [
+        ("cbSize", wintypes.UINT),
+        ("dwTime", wintypes.DWORD),
+    ]
+
+
+def get_idle_seconds() -> float:
+    """Return elapsed seconds since the last keyboard/mouse/touch interaction."""
+    user32 = ctypes.windll.user32
+    kernel32 = ctypes.windll.kernel32
+
+    last_input_info = LASTINPUTINFO()
+    last_input_info.cbSize = ctypes.sizeof(LASTINPUTINFO)
+
+    if not user32.GetLastInputInfo(ctypes.byref(last_input_info)):
+        raise OSError("GetLastInputInfo failed")
+
+    tick_count = kernel32.GetTickCount()
+    elapsed_ms = tick_count - last_input_info.dwTime
+    return max(elapsed_ms / 1000.0, 0.0)
---
+++ client/state_machine.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class ClientState(str, Enum):
+    ACTIVE = "ACTIVE"
+    IDLE_PENDING = "IDLE_PENDING"
+    PLAYING = "PLAYING"
+    RETURNING = "RETURNING"
